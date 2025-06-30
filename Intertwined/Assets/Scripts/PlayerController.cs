a5e3d0f02b11cfc55813fb4fa76eeba2554e8e7b
BLowdc <lowbh@dulwich.org.uk>
2025-06-30 12:29:10 +0100

    Tilemap, mana/health system

--- Intertwined/Assets/Scripts/PlayerController.cs
+++ Intertwined/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,22 @@ using UnityEngine.InputSystem.Utilities;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
-
+    [SerializeField] private float moveSpeed = 3f;
+    private bool isSprinting;
+    public bool IsSprinting => isSprinting;
     private PlayerControls playerControls;
+    private PlayerStats playerStats;
     private Vector2 movement;
     private Rigidbody2D rb;
     private int sprintMultiplier = 2;
-
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
+        playerStats = GetComponent<PlayerStats>();
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -28,14 +32,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerInput();
-        
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+
+        Debug.Log(playerStats.Mana);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && playerStats.Mana > playerStats.MaxMana * 0.25f)
         {
             moveSpeed *= sprintMultiplier;
+            isSprinting = true;
+            spriteRenderer.color = Color.green;
+
         }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || playerStats.Mana <= 0)
         {
-            moveSpeed /= sprintMultiplier;
+            moveSpeed = 3f;
+            isSprinting = false;
+            spriteRenderer.color = Color.white;
         }
     }
     private void FixedUpdate()
@@ -46,7 +57,6 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>(); 
-        // Debug.Log($"Movement Input: {movement * (moveSpeed * Time.fixedDeltaTime)}");
     }
     
     private void Move()
---
+++ Intertwined/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,39 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int maxMana = 50;
+    public int MaxMana => maxMana;
+    private int health;
+    private float mana;
+    public float Mana => mana;
+    [SerializeField] private float manaRegenRate = 5f;
+    [SerializeField] private float manaDrainRate = 10f;
+    private PlayerController playerController;
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        health = maxHealth;
+        mana = maxMana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.IsSprinting && mana > 0)
+        {
+            mana -= manaDrainRate * Time.deltaTime;
+            if (mana < 0) mana = 0; 
+            // Debug.Log($"Draining Mana: {mana}");
+        }
+
+        if (!playerController.IsSprinting && mana < maxMana)
+        {
+            mana += manaRegenRate * Time.deltaTime;
+            if (mana > maxMana) mana = maxMana;
+            // Debug.Log($"Regening Mana: {mana}");
+        }
+    }
+}
