d78227bba90edf5f264aeca3b9a71a70cec96079
BLowdc <lowbh@dulwich.org.uk>
2026-03-25 09:39:29 +0000

    comments

--- Intertwined/Assets/Scripts/BossRoomCloseGate.cs
+++ Intertwined/Assets/Scripts/BossRoomCloseGate.cs
@@ -4,6 +4,7 @@ public class BossRoomCloseGate : MonoBehaviour
 {
     public GameObject gate;
 
+    // when players step goes through the gate, close gate
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
--- Intertwined/Assets/Scripts/GateManager.cs
+++ Intertwined/Assets/Scripts/GateManager.cs
@@ -16,7 +16,7 @@ public class GateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if  (plate1.IsPressed && plate2.IsPressed && plate3.IsPressed)
+        if (plate1.IsPressed && plate2.IsPressed && plate3.IsPressed)
         {
             gate.SetActive(false);
         }
